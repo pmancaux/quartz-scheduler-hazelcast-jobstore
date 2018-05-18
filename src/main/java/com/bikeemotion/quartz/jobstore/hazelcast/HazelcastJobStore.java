e696a9e824a590539113a412501bcc09cd0124c0
Pierre-Alexandre <ext.sopra-steria.pierre-alexandre.mancaux@sncf.fr>
2018-05-18 14:21:46 +0200

    commit de rebase des tests

--- src/main/java/com/bikeemotion/quartz/jobstore/hazelcast/HazelcastJobStore.java
+++ src/main/java/com/bikeemotion/quartz/jobstore/hazelcast/HazelcastJobStore.java
@@ -59,7 +59,7 @@ import static org.quartz.impl.matchers.StringMatcher.StringOperatorName.EQUALS;
 
 /**
  *
- * @author Flavio Ferreira test 2
+ * @author Flavio Ferreira test 4
  *
  *         Thanks Antoine Méausoone for starting the work.
  */
